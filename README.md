d658c4ea9dd23397a936ddbf7a7038083a34021c
kle-pra <klemen.pravdic@gmail.com>
2023-12-06 12:47:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# eslint-vitest
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/kle-pra/eslint-vitest)
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export enum TestEnum {
+  A = '1', B = '2'
+}
+
+export const allTestValues = Object.values(TestEnum);
